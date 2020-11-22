4cb41ae6113c43112404da74b3e09f7d34494fde
voittamatonkulho <kim.backman@edu.turkuamk.fi>
2020-11-22 19:42:16 +0200

    Reduced code repetition
    Created class CountdownLabel for the break and screen time labels as
    they shared several properties. Combined the two timers into one.

---
+++ screentimebreak/CountdownLabel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace screentimebreak {
+    class CountdownLabel : Label {
+        public CountdownLabel(int fontSize) : base() {
+            ForeColor = Color.White;
+            AutoSize = true;
+            Font = new Font("Calibri", fontSize);
+        }
+    }
+}
--- screentimebreak/TimerOverlayForm.cs
+++ screentimebreak/TimerOverlayForm.cs
@@ -24,38 +24,34 @@ namespace screentimebreak {
         private Label screenTimeLabel;
         private Label breakTimeLabel;
 
-        public TimerOverlayForm(int screenTimeMinutes = 0, int screenTimeSeconds = 5, int breakTimeMinutes = 2, int breakTimeSeconds = 30) {
+        public TimerOverlayForm(int screenTimeMinutes = 0, int screenTimeSeconds = 5, int breakTimeMinutes = 0, int breakTimeSeconds = 8) {
 
             this.screenTimeMinutes = screenTimeMinutes;
             this.screenTimeSeconds = screenTimeSeconds;
             this.breakTimeMinutes = breakTimeMinutes;
             this.breakTimeSeconds = breakTimeSeconds;
 
-            screenTimeLabel = new Label();
+            // Labels
+            screenTimeLabel = new CountdownLabel(30);
             screenTimeLabel.Location = new Point(500, 500);
-            screenTimeLabel.ForeColor = Color.White;
-            screenTimeLabel.Font = new Font("Calibri", 40);
-            screenTimeLabel.AutoSize = true;
             screenTimeLabel.Visible = true;
 
-
-            breakTimeLabel = new Label();
+            breakTimeLabel = new CountdownLabel(50);
             breakTimeLabel.Location = new Point(1000, 500);
-            breakTimeLabel.ForeColor = Color.White;
-            breakTimeLabel.Font = new Font("Calibri", 40);
-            breakTimeLabel.AutoSize = true;
             breakTimeLabel.Visible = false;
-
 
             this.Controls.Add(screenTimeLabel);
             this.Controls.Add(breakTimeLabel);
 
             InitializeComponent();
+
+            // Visual properties
             FormBorderStyle = FormBorderStyle.None;
             Opacity = 0.70;
             BackColor = Color.Black;
             TransparencyKey = BackColor;
             TopMost = true;
+
             setSizeToAllScreens();
 
             // Enable clicking through the form
@@ -91,58 +87,61 @@ namespace screentimebreak {
         // Start the main timer loop
         private void startMainLoop() {
 
-            int screenTime = (this.screenTimeMinutes * 60) + this.screenTimeSeconds;
-            int decrementingScreenTime = screenTime;
-
-            int breakTime = (this.breakTimeMinutes * 60) + this.breakTimeSeconds;
-            int decrementingBreakTime = breakTime;
-
-            Timer screenTimer = new Timer();
-            screenTimer.Interval = 1000;
-            screenTimer.Enabled = true;
-
-            Timer breakTimer = new Timer();
-            breakTimer.Interval = 1000;
-            breakTimer.Enabled = false;
-
-            screenTimer.Tick += new EventHandler(screenTimer_Tick);
-            breakTimer.Tick += new EventHandler(breakTimer_Tick);
+            int screenTime = (screenTimeMinutes * 60) + screenTimeSeconds;
+            int breakTime = (breakTimeMinutes * 60) + breakTimeSeconds;
 
 
-            void screenTimer_Tick(object sender, EventArgs e) {
-                if (decrementingScreenTime == 0) {
-                    enterBreakMode();
+            int countdownSecondsLeft = screenTime;
+            bool screenMode = true;
+
+            Timer countdownTimer = new Timer();
+            countdownTimer.Interval = 1000;
+            countdownTimer.Enabled = true;
+
+            countdownTimer.Tick += new EventHandler(countdownTimer_Tick);
+
+            void countdownTimer_Tick(object sender, EventArgs e) {
+
+                if (countdownSecondsLeft < 0) {
+                    if (screenMode) {
+                        enterBreakMode();
+                    }
+                    else {
+                        enterScreenMode();
+                    }
                 }
-                screenTimeLabel.Text = decrementingScreenTime.ToString();
-                screenTimeLabel.Refresh();
-                decrementingScreenTime--;
-            }
 
-            void breakTimer_Tick(object sender, EventArgs e) {
-                if (decrementingBreakTime == 0) {
-                    enterScreenMode();
+                if (screenMode) {
+                    screenTimeLabel.Text = countdownSecondsLeft.ToString();
+                    screenTimeLabel.Refresh();
                 }
-                breakTimeLabel.Text = decrementingBreakTime.ToString();
-                breakTimeLabel.Refresh();
-                decrementingBreakTime--;
+                else {
+                    breakTimeLabel.Text = countdownSecondsLeft.ToString();
+                    breakTimeLabel.Refresh();
+                }
+
+                countdownSecondsLeft--;
+
             }
 
             void enterScreenMode() {
-                breakTimer.Stop();
+                countdownTimer.Stop();
+                screenMode = true;
                 TransparencyKey = BackColor;
                 breakTimeLabel.Visible = false;
                 screenTimeLabel.Visible = true;
-                decrementingBreakTime = breakTime;
-                screenTimer.Start();
+                countdownSecondsLeft = screenTime;
+                countdownTimer.Start();
             }
 
             void enterBreakMode() {
-                screenTimer.Stop();
+                countdownTimer.Stop();
+                screenMode = false;
                 TransparencyKey = Color.Magenta;
                 screenTimeLabel.Visible = false;
                 breakTimeLabel.Visible = true;
-                decrementingScreenTime = screenTime;
-                breakTimer.Start();
+                countdownSecondsLeft = breakTime;
+                countdownTimer.Start();
             }
 
         }
